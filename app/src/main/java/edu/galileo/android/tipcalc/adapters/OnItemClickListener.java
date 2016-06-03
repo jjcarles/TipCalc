23aa9c15bd07ec78f64feb649cffeb487d7d2f80
Joan Carles Jimenez <jjcarles@jocajica.com>
2016-06-04 00:57:02 +0200

    Implementada la funcionalitat dels items de la llista.

---
+++ app/src/main/java/edu/galileo/android/tipcalc/adapters/OnItemClickListener.java
@@ -0,0 +1,10 @@
+package edu.galileo.android.tipcalc.adapters;
+
+import edu.galileo.android.tipcalc.models.TipRecord;
+
+/**
+ * Created by Joan Carles Jiménez on 04/06/2016.
+ */
+public interface OnItemClickListener {
+    void onItemClick(TipRecord record);
+}
