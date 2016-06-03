dd3b1136c29576955fef6a6e94889ba40f9da206
Joan Carles Jimenez <jjcarles@jocajica.com>
2016-06-03 23:41:16 +0200

    Creat el adapter de la llista.

---
+++ app/src/main/java/edu/galileo/android/tipcalc/models/TipRecord.java
@@ -0,0 +1,47 @@
+package edu.galileo.android.tipcalc.models;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Joan Carles Jiménez on 03/06/2016.
+ */
+public class TipRecord {
+
+    private double bill;
+    private int tipPercentage;
+    private Date timeStamp;
+
+    public double getBill() {
+        return bill;
+    }
+
+    public void setBill(double bill) {
+        this.bill = bill;
+    }
+
+    public int getTipPercentage() {
+        return tipPercentage;
+    }
+
+    public void setTipPercentage(int tipPercentage) {
+        this.tipPercentage = tipPercentage;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public double getTip() {
+        return bill * (tipPercentage / 100d);
+    }
+
+    public String getDateFormatted() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM dd,yyyy HH:mm");
+        return simpleDateFormat.format(timeStamp);
+    }
+}
