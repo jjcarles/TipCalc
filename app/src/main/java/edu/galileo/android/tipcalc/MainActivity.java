d58fb080d4147096d3d75a288d37c752e0e9da16
jcarles <jcarles@kitchenmaster.es>
2016-06-03 09:32:38 +0200

    Butterknife. Cal instal.lar el plug-in de Zelezny per integrar el Butterknife a Android Studio.

--- app/src/main/java/edu/galileo/android/tipcalc/MainActivity.java
+++ app/src/main/java/edu/galileo/android/tipcalc/MainActivity.java
@@ -1,18 +1,47 @@
 package edu.galileo.android.tipcalc;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.inputBill)
+    EditText inputBill;
+    @BindView(R.id.btnSubmit)
+    Button btnSubmit;
+    @BindView(R.id.inputPercentage)
+    EditText inputPercentage;
+    @BindView(R.id.btnIncrease)
+    Button btnIncrease;
+    @BindView(R.id.btnDecrease)
+    Button btnDecrease;
+    @BindView(R.id.btnClear)
+    Button btnClear;
+    @BindView(R.id.txtTip)
+    TextView txtTip;
+
+    private final static int TIP_STEP_CHANGE = 1;
+    private final static int DEFAULT_TIP_PERCENTAGE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -30,8 +59,25 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @OnClick(R.id.btnSubmit)
+    public void handleClickSubmit() {
+        Log.e(getLocalClassName(), "click en submit");
+        hideKeyboard();
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        try {
+            inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+        catch (NullPointerException npe) {
+            Log.e(getLocalClassName(), Log.getStackTraceString(npe));
+        }
+    }
+
     private void about() {
-        TipCalcApp app = (TipCalcApp)getApplication();
+        TipCalcApp app = (TipCalcApp) getApplication();
         String strUrl = app.getAboutUrl();
 
         Intent intent = new Intent(Intent.ACTION_VIEW);
