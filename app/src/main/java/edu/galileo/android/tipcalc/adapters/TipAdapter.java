f257b60d2e0367d7f1696bf8a3c94bd624f6a35d
Joan Carles Jimenez <jjcarles@jocajica.com>
2016-06-04 22:42:29 +0200

    Corretgit error amb el item_row.

--- app/src/main/java/edu/galileo/android/tipcalc/adapters/TipAdapter.java
+++ app/src/main/java/edu/galileo/android/tipcalc/adapters/TipAdapter.java
@@ -35,6 +35,7 @@ public class TipAdapter extends RecyclerView.Adapter<TipAdapter.ViewHolder> {
         this.dataset = dataset;
         this.onItemClickListener = onItemClickListener;
     }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row, parent, false);
